16d3b8d4a53dd5dd953b89b15e7fea0a080efe4f
Your Name <vroy101@gmail.com>
2023-05-30 14:05:22 -0400

    remove comments in repro

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,6 @@
 "use server";
 
 export default async function() {
-  // From Google Play store:
-  // curve images + shadow
-  // gray outline + no grid padding (horizontal margins can be narrow)
-  // decent spacing under title
-  // no spacing for subtext
-  // extra padding on bottoms of items
-  // do not change item size depending on screen width
-  // center grid on large screens / center always
-  // on mobile (single column) => have enough padding
-
-  // From Github:
-  // - when you have limited content, show unnecessary header text
-
-  // From tailwindcss:
-  // - align header w/ grid
-  // - header doesn't need to be a different color, use a thin line instead
-  // - use inter (done by default?)
-
-  // From langshift:
-  // logo tricks => contrast keyword vs generic word
-
   async function createGame(formData) {
     "use server";
     const obj = Object.fromEntries(formData);
